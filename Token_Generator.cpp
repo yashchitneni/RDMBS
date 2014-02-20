cadb25e4af34cd377a6a9ac18db44f49d8e3d816
jedioli <jedi_oli@tamu.edu>
2014-02-20 15:05:33 -0600

    more token stuff, update README for more universal info

--- Token_Generator.cpp
+++ Token_Generator.cpp
@@ -6,7 +6,70 @@
 
 #include "Token_Generator.h"
 
+std::string Token_Generator::create_league(string name, string country, string sponsor) {
+    std::stringstream token_stream;
+    
+    //creating table implementation
+    /*
+    token_stream << "CREATE TABLE ";
+    token_stream << name;
+    token_stream << " (name VARCHAR(20), country VARCHAR(10), sponsor VARCHAR(10), ";
+    */
+    
+    
+    //league -> NAME, country, sponsor, current_champ, num_teams
+    //caps are primary keys
+    
+    
+    token_stream << "INSERT INTO " << /* leagues table name << */ " VALUES FROM (\"" << name;
+    token_stream << "\", \"" << country << "\", \"" << sponsor << "\", \"";
+    token_stream << /* current champ to null */ " " << "\", 0);";
+    
+    return token_stream.str();
+}
+
+std::string create_team(string name, string league, string city, string sponsor, int year_founded, string manager, string kit) {
+    std::stringstream token_stream;
+    
+    //team -> NAME, city, sponsor, year_founded, MANAGER, kit, points, goals, assists, cards
+    
+    token_stream << "INSERT INTO " << /* teams table name << */ " VALUES FROM (\"" << name;
+    token_stream << "\", \"" << city << "\", \"" << sponsor << "\", ";
+    token_stream << year_founded << ", \"" << manager << "\", \"" << kit << "\"";
+    token_stream << " 0, 0, 0, 0);";
+    
+    return token_stream.str();
+}
+
+std::string create_player(string name, int number, string team, string position) {
+    //player -> name, JERSEY_NUM, TEAM, position, goals, assists, cards, starter
+    
+    std::stringstream token_stream;
+    
+    token_stream << "INSERT INTO " << /* players table name << */ " VALUES FROM (\"" << name;
+    token_stream << "\", " << number << ", \"" << team << "\", \"";
+    token_stream << position << "\", 0, 0, 0, \"no\");";    //HOW FORMAT STARTERS??
+    
+    return token_stream.str();
+}
+
+std::string form_roster(string team_name) {
+    std::stringstream token_stream;
+    
+    token_stream << team_name << " <- select (" << /* team header name <<*/ " == \"" << team_name;
+    token_stream << "\") "  /* << players table */ << ";";
+    
+    return token_stream.str();
+}
+
+std::string form_league(string league_name) {
+    std::stringstream token_stream;
+    
+    token_stream << league_name << " <- select (" << /* team header name <<*/ " == \"" << team_name;
+    token_stream << "\") "  /* << players table */ << ";";
+    
+    return token_stream.str();
+}
 
 
 
-void Token_Generator::create_league(string name, string country, string sponsor) {}
--- Token_Generator.h
+++ Token_Generator.h
@@ -6,6 +6,8 @@
 
 /*
  to do:
+    initialize leagues, teams, players tables at beginning
+ 
     nail down menu options
         get specifics of internals of menu options
     implement menu option functions
@@ -23,6 +25,18 @@
         framework
     KEEP DEV LOG up to date
  */
+
+//ordering of tuples
+//league -> NAME, country, sponsor, current_champ, num_teams
+//team -> NAME, league, city, sponsor, year_founded, MANAGER, kit, points, goals, assists, cards
+//player -> name, JERSEY_NUM, TEAM, position, goals, assists, cards, starter
+    //right now, uppercase attrs are primary keys
+    //use update to change team(key)
+
+//STANDARDIZED table header titles
+//_LEAGUE -> league_name, country, sponsor, current_champ, num_teams
+//_TEAM -> team_name, league, city, sponsor, year_founded, manager, kit, points, goals, assists, cards
+//_PLAYER -> player_name, jersey_num, team, position, goals, assists, cards, starter
 
 
 #include <iostream>
@@ -42,39 +56,50 @@ public:
     
     //option methods
     
-    void create_league(string name, string country, string sponsor);    //more data?
+    static std::string create_league(string name, string country, string sponsor);    //more data?
+        //creates tuple to insert into leagues
         //initialize current_champion to NULL
         //initialize num of teams to 0
     
-    void create_team(string name, string city, string sponsor, int year_founded, string manager, string kit);
+    static std::string create_team(string name, string league, string city, string sponsor, int year_founded, string manager, string kit);
         //initialize "points" (use point system for wins/losses/ties), goals_scores, assists, and cards to 0 within
     
-    void form_roster(string team_name, vector<string> players);
-    
-    void form_league(string league_name, vector<string> teams);
-    
-    
-    
-    void create_player(string name, int number, string position);   //also include "invisible" attributes, like goals and assists
+    static std::string create_player(string name, int number, string team, string position);   //also include "invisible" attributes, like goals and assists
         //goals, assists, cards = 0
         //starting = no
+        //can pass "" for team to make free agent
+            //probably not...
+    
+    static std::string form_roster(string team_name);   //don't need this... ", vector<string> players"
+    
+    static std::string form_league(string league_name); //don't need this... ", vector<string> teams"
    
-    void play_game(/*data, like which teams, etc.*/);   //THESE ARE FINAL STATS, after the game.
-    void play_game(string team1, string team2, int num_goals1, int num_goals2, vector<string> goal_scorers1, vector<string> goal_scorers2, int assists1, int assists2, vector<string> assisters1, vector<string> assisters2, int cards1, int cards2, vector<string> foulers1, vector<string> foulers2); //pass array of strings with same size as number of goals
+    static std::string play_game(/*data, like which teams, etc.*/);   //THESE ARE FINAL STATS, after the game.
+    static std::string play_game(string team1, string team2, int num_goals1, int num_goals2, vector<string> goal_scorers1, vector<string> goal_scorers2, int assists1, int assists2, vector<string> assisters1, vector<string> assisters2, int cards1, int cards2, vector<string> foulers1, vector<string> foulers2); //pass array of strings with same size as number of goals
         //look at contents of vector to determine how many for which players
         //decide how vector is structure later
     
     
-    void view_player_stats(string player_name, string team_name /* this can change */);
+        //update scores and goals, etc. for ONLY "big three" tables!
+        //and REGENERATE roster tables for EVERY show!
+    
+    
+    static std::string view_player_stats(string player_name, string team_name /* this can change */);
     //not sure exactly what identifying info we need...
     //maybe access the giant Player database and use unique IDs for ALL players? just an idea.
     
-    void view_team_stats(string team_name, string league_name /* team key */);
+    static std::string view_team_stats(string team_name, string league_name /* team key */);
     
-    void view_league_stats(string league_name /* league key */);
+    static std::string view_league_stats(string league_name /* league key */);
         //compute current_champion within
     
-    void transfer_player(string player_name, string team_from, string team_to /*data, like player to transfer, from/to, etc.*/);
+    static std::string view_team_roster(string team_name);
+        //for displaying roster
+    
+    static std::string view_league_teams(string league_name);
+        //for displaying list of teams in league
+    
+    static std::string transfer_player(string player_name, string team_from, string team_to /*data, like player to transfer, from/to, etc.*/);
         //CANNOT HAVE CONFLICTING JERSEY NUMBERS so need to give him new one if conflicts.
         //need to update starting roster for each team to maintain at 11
     
@@ -89,10 +114,10 @@ public:
     
     
     //dispense method, to pour into parser
-    std::string dispense();
+    //std::string dispense();
     
 private:
-    std::stringstream token_stream;
+    //std::stringstream token_stream;
     
     //sub-methods, to be called by individual options
         //are these necessary?
